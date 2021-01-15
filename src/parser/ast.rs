1dc3f9bd5a0d38db36f13107f6fea2b562d3c348
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-01-14 19:16:08 -0800

    Fix add AST

--- src/parser/ast.rs
+++ src/parser/ast.rs
@@ -1,11 +1,6 @@
 #[derive(Clone, Debug, PartialEq)]
-pub enum Operator {
-    Add,
-}
-
-#[derive(Clone, Debug, PartialEq)]
 pub enum AST<'a> {
     Call(&'a str, Vec<&'a str>),
     Num(&'a str), // We don't care about the value of the int, desmos can figure that out
-    Add(Operator, Box<AST<'a>>, Box<AST<'a>>),
+    Add(Box<AST<'a>>, Box<AST<'a>>),
 }
