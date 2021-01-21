66272aceecabf0fb78bb8470d3c9bc35a8335547
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-01-21 15:04:54 -0800

    Remove unused code

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,6 @@
 pub mod interpreter;
 pub mod parser;
 
-/// Type-erased errors.
-pub type BoxError = std::boxed::Box<
-    dyn std::error::Error + std::marker::Send + std::marker::Sync, // needed for threads
->;
-
 pub fn parse(inp: &str) {
     println!("{:#?}", parser::parser::parse(inp));
 }
