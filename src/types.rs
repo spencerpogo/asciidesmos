4fbd9657981b2b94a8e3036d9ad20c413c4128bb
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-02-21 18:21:38 -0800

    Remove old uneeded AssertionError

--- src/types.rs
+++ src/types.rs
@@ -1,21 +1,6 @@
 use pest::Span as PestSpan;
 use std::fmt;
 
-#[derive(Debug, Clone)]
-pub struct AssertionError;
-
-// Generation of an error is completely separate from how it is displayed.
-// There's no need to be concerned about cluttering complex logic with the display style.
-//
-// Note that we don't store any extra info about the errors. This means we can't state
-// which string failed to parse without modifying our types to carry that information.
-impl fmt::Display for AssertionError {
-    #[cfg(not(tarpaulin_include))]
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "assertion error while processing tokens")
-    }
-}
-
 // Expression is a component of a statement
 #[derive(Clone, Debug, PartialEq)]
 pub enum Expression<'a> {
