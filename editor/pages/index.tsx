3bcd6eb96ed1d2220139eab689335469a2369cab
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2022-06-09 15:58:32 +0000

    Implement test RPC transport

--- editor/pages/index.tsx
+++ editor/pages/index.tsx
@@ -2,6 +2,8 @@ import init, { js_closure_test } from "desmosc-wasm";
 import { useEffect, useState } from "react";
 import { basicSetup } from "codemirror";
 import CodeMirror from "@uiw/react-codemirror";
+import { Transport } from "@open-rpc/client-js/build/transports/Transport";
+import { JSONRPCRequestData } from "@open-rpc/client-js/build/Request";
 
 enum State {
   Loading,
@@ -9,9 +11,31 @@ enum State {
   Ready,
 }
 
+export class MyTransport extends Transport {
+  public log: (msg: string) => void;
+  constructor(log) {
+    super();
+    this.log = log;
+  }
+  public async connect(): Promise<void> {
+    this.log("connect");
+  }
+  public close() {
+    this.log("close");
+  }
+  public async sendData(
+    data: JSONRPCRequestData,
+    timeout?: number | null
+  ): Promise<void> {
+    this.log(JSON.stringify(data));
+  }
+}
+
 const Main = () => {
   const [state, setState] = useState(State.Loading);
   const [value, setValue] = useState("test");
+  const [logs, setLogs] = useState([]);
+  
   useEffect(() => {
     init()
       .then(() => {
@@ -26,7 +50,12 @@ const Main = () => {
   if (state == State.Error)
     return <p>Error loading WASM. Check console for details.</p>;
   if (state == State.Loading) return <p>Loading...</p>;
-  return <CodeMirror value={value} height="200px" extensions={[basicSetup]} />;
+  return (
+    <>
+      <CodeMirror value={value} height="200px" extensions={[basicSetup]} />
+      <pre>{logs.join("\n")}</pre>
+    </>
+  );
 };
 
 const IndexPage = () => {
