d6108171a83688d5bc47143eede4862b2a7c8f40
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2022-06-09 06:39:06 +0000

    Properly load and use editor WASM

--- editor/pages/index.tsx
+++ editor/pages/index.tsx
@@ -1,15 +1,34 @@
-import init, { greet } from "desmosc-wasm";
+import init, { greet, try_eval } from "desmosc-wasm";
 import { useEffect, useState } from "react";
 
-const IndexPage = () => {
-  const [ready, setReady] = useState(false);
+enum State {
+  Loading,
+  Error,
+  Ready,
+}
+
+const Main = () => {
+  const [state, setState] = useState(State.Loading);
   useEffect(() => {
-    init().then(() => setReady(true));
+    init()
+      .then(() => setState(State.Ready))
+      .catch((e) => {
+        console.error(e);
+        setState(State.Error);
+      });
   }, []);
+  if (state == State.Error)
+    return <p>Error loading WASM. Check console for details.</p>;
+  if (state == State.Loading) return <p>Loading...</p>;
+  const res = try_eval("1+1;");
+  return <pre>{res.output}</pre>;
+};
+
+const IndexPage = () => {
   return (
     <>
       <h1>Test App</h1>
-      <pre>{ready ? greet("rust + next.js") : "Loading..."}</pre>
+      <Main />
     </>
   );
 };
