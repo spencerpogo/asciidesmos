e57a02c1c8f2c6621d757692bbf380f90a0c1e7d
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2022-09-18 21:49:41 -0700

    Fix ambiguous import

--- compiler/src/lib.rs
+++ compiler/src/lib.rs
@@ -6,6 +6,6 @@ mod import;
 mod stdlib;
 mod types;
 
+pub use crate::compiler::{compile_stmt, compile_stmts, stmts_to_graph};
 pub use crate::types::{Context, Loader};
 pub use ast::LStatements; // required for loader signatures
-pub use compiler::{compile_stmt, compile_stmts, stmts_to_graph};
